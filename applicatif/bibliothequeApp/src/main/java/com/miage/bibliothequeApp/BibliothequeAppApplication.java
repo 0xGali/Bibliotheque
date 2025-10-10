91bd15e0ffdfe93ad46f7f43fa1037078f31eca3
Maximilien Bleeker <maximilien.bleeker9@etu.univ-lorraine.fr>
2025-10-10 12:24:21 +0200

    Correction erreur repository

--- applicatif/bibliothequeApp/src/main/java/com/miage/bibliothequeApp/BibliothequeAppApplication.java
+++ applicatif/bibliothequeApp/src/main/java/com/miage/bibliothequeApp/BibliothequeAppApplication.java
@@ -2,8 +2,10 @@ package com.miage.bibliothequeApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.miage.bibliothequeApp.repository")
 public class BibliothequeAppApplication {
 
 	public static void main(String[] args) {
