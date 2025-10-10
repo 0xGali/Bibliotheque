8bfb765430b7cf991b714c5d8743a5b7be29a374
Maximilien Bleeker <maximilien.bleeker9@etu.univ-lorraine.fr>
2025-10-10 12:04:39 +0200

    Ajout fonction getOeuvres

--- applicatif/bibliothequeApp/src/main/java/com/miage/bibliothequeApp/controller/OeuvreController.java
+++ applicatif/bibliothequeApp/src/main/java/com/miage/bibliothequeApp/controller/OeuvreController.java
@@ -1,5 +1,20 @@
 package com.miage.bibliothequeApp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.miage.bibliothequeApp.model.Oeuvre;
+import com.miage.bibliothequeApp.service.OeuvreService;
+
+@RestController
 public class OeuvreController {
 
+    @Autowired
+    OeuvreService oeuvreService;
+
+    @GetMapping("/getOeuvres")
+    public Iterable<Oeuvre> getOeuvres(){
+        return oeuvreService.getOeuvres();
+    }
 }
--- applicatif/bibliothequeApp/src/main/java/com/miage/bibliothequeApp/service/OeuvreService.java
+++ applicatif/bibliothequeApp/src/main/java/com/miage/bibliothequeApp/service/OeuvreService.java
@@ -1,5 +1,21 @@
 package com.miage.bibliothequeApp.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.miage.bibliothequeApp.model.Oeuvre;
+import com.miage.bibliothequeApp.repository.OeuvreRepository;
+
+import lombok.Data;
+
+@Data
+@Service
 public class OeuvreService {
 
+    @Autowired
+    private OeuvreRepository oeuvreRepository;
+
+    public Iterable<Oeuvre> getOeuvres() {
+        return oeuvreRepository.findAll();
+    }
 }
