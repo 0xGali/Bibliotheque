65eacd98e10ba45871dc32a8a2700273c39c04d1
Maximilien Bleeker <maximilien.bleeker9@etu.univ-lorraine.fr>
2025-10-10 11:45:31 +0200

    Oeuvre

--- applicatif/bibliothequeApp/src/main/java/com/miage/bibliothequeApp/model/Oeuvre.java
+++ applicatif/bibliothequeApp/src/main/java/com/miage/bibliothequeApp/model/Oeuvre.java
@@ -1,5 +1,16 @@
 package com.miage.bibliothequeApp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="Oeuvre")
 public class Oeuvre {
-    
+    @Id
+    private String titre;
+    private String auteur;
+    private String editeur;
 }
